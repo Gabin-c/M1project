a735ba7548acbe44b67dd0bca0e7bc1b5fc33268
David.Gallien <davidgallien1997@gmail.com>
2020-05-09 20:27:41 +0200

    miam

---
+++ RNA-seqDE/function_dds.R
@@ -0,0 +1,248 @@
+library(DESeq2)
+library(Biobase)
+library(gplots)
+library(RColorBrewer)
+library(shiny)
+library(tidyverse)
+library(NMF)
+library(ggrepel)
+
+
+### Preamble ----
+### All of this function can be use after running a DESeq2 workflow.
+### You need a DESeq2 dataset to be able to run it.
+###   - DESeq2 : count table after run counts() on yout DESeq2 dataset
+###      - depth()
+###      - count_distribution()
+###      - plotcount()
+###   - DESeq2 : results object after run DESeq() on your DESeq2 dataset and results() onr DESeq() object 
+###      - maplot()
+###      - volcanoplot()
+###      - number_of_DE 
+###   - DESeq2 : after run DESeq() on your DESeq2 datasetn and do vst() or rlogtransformation() on DESeq() object
+###      - pca()
+###      - heatmap()
+###      - clustering_heatmap()
+###      - dispersion()
+
+### Depth plot ----
+### depth return a barplot of depth sample with ggplot library  
+### depth need two argument
+###   - dds which is a count table of a RNAseq experience which have in column : sample, and in row :  gene
+###   - breaksize which is width of the bar
+depth.plot <- function(dds.count,break.width=1){
+  depth <- as.data.frame(colSums(dds.count))
+  depth$Sample <- row.names(depth)
+  
+  return(ggplot(depth, aes( x=Sample ,y=depth[,1]))+ 
+           geom_bar(stat="identity",fill=brewer.pal(n=length(depth$Sample),name="YlGn"),width = break.width)+
+           labs(title = "Depth of each sample", x="Samples", y="Depth")+theme_bw()+
+           theme(plot.title = element_text(face = "bold", size= 18)) +
+           theme(axis.title.x = element_text(size=14)) +
+           theme(axis.title.y = element_text(size=14)))
+}
+
+
+### Count distribution plot ----
+### count_distribution return an histogram of count values distribution in the log(count+1) (to facilitate vizualisation) format for one sample
+### count_distribution need five arguments
+###   - dds which is a count table of an RNAseq experience which have in column : sample, and in row :  gene
+###   - breaksize which is width of histogram bar
+###   - sample for which we display the count distribution
+###   - min which is the min of x axis 
+###   - max which is the max of x axis
+
+count.distribution.plot <- function(dds.count, sample,x.min=0,x.max=14,break.width=1){
+  
+  return(ggplot(data=dds.count, aes(log(dds.count[,sample]+1))) + 
+           geom_histogram(breaks=seq(x.min,x.max,break.width),position="identity",alpha=0.5,fill="darkcyan", color="dodgerblue1")+
+           theme_classic() +
+           labs(title=sample, x="Counts values (number of reads by gene) in log(count+1)",y="Counts frequencies") +
+           theme(plot.title = element_text(face = "bold", size= 18)) +
+           theme(axis.title.x = element_text(size=14)) +
+           theme(axis.title.y = element_text(size=14))
+  )
+}
+
+
+### Dispersion plot ----
+### dispersion return plot obtained by DESeq2::plotDispEsts() function of DESeq2 package
+### dispersion just need one argument : a DESeq() object
+dispersion <- function(dds){
+  DESeq2::plotDispEsts(dds, main= "Relationship between dispersion and counts means")
+}
+### nummber of differemtial express gene ----
+### number.DE.gene return a table of DE results with number of TRUE,FALSE and NA in function of pvalue
+### number.DE.gene need two arguments
+###   - dds.result which is a results table obtain by function results() on a DESeq() object
+###   - p.val which is pvalue accept un DE analysis, padje is set at 0.05
+
+number.DE.gene <- function(dds.result,p.val = 0.05){
+  tb.DE <- as.data.frame(table(dds.result$padj <= p.val ,useNA="always"))
+  colnames(tb.DE) = c("DE","Genes")
+  return(tb.DE)
+}
+
+
+
+
+
+
+### Plotcount ----
+### plotcount return a point plot of read count for one gene in each sample
+### plotcount need two argument
+###   - dds.count which is a count table of a RNAseq experience which have in column : sample, and in row : gene
+###   - gene which is gene which we want to display read count for each sample
+
+gene.count.plot <- function(dds.count,gene){
+  dds1 <- dds.count
+  dds1[,"name"] <- row.names(dds1)
+  return(
+    ggplot(dds1, aes(x=dds1[,"name"], y=dds1[,gene])) + 
+      geom_point(size=4,aes(colour=factor(name))) + 
+      geom_segment(aes(x=dds1[,"name"], xend=dds1[,"name"], y=0, yend=dds1[,gene]),linetype="dotdash")+ 
+      theme(axis.text.x = element_blank() )+ 
+      labs(title=paste("Count of",gene,  "for each sample"),x="Samples",y="Counts")+ 
+      guides(color= guide_legend(title = "Sample", override.aes = list(size=5))) +
+      theme(plot.title = element_text(face = "bold", size= 18)) +
+      theme(axis.title.x = element_text(size=14)) +
+      theme(axis.title.y = element_text(size=14)) +
+      theme(legend.text=element_text(size=13)) +
+      theme(legend.title=element_blank())
+  )
+}
+
+### Maplot ---- 
+### maplot return a MA plot of DE
+### maplot need two arguments
+###   - dds.results which is a results table obtain by function results() on a DESeq() object
+###   - p.val which is pvalue accept un DE analysis, padje is set at 0.05
+ma.plot <- function(dds.results,p.val=0.05){
+  dds.res <- dds.results %>% mutate(sig=padj<p.val)
+  return(ggplot(dds.res, aes(x = baseMean, y = log2FoldChange, col = sig)) + 
+           geom_point() + 
+           scale_x_log10() +
+           geom_hline(yintercept = 0, linetype = "dashed",color = "black") + 
+           theme_bw() +
+           scale_colour_discrete(name="",labels=c("Not significative", "Significative", "NA"))+
+           guides(color = guide_legend(override.aes = list(size=5))) +
+           theme(legend.text=element_text(size=13))+
+           theme(axis.title.x = element_text(size=14)) +
+           theme(axis.title.y = element_text(size=14)))
+}
+
+
+### VolcanonPlot ---- 
+### volcano.plot generate a volcano plot of DE
+### volcano.plot need 8 arguments
+###   - dds.results which is a results table obtain by function results() on a DESeq() object
+###   - is.anno if there is an annotation file
+###   - anno an annotation 
+###   - p.val which is pvalue accept un DE analysis, padje is set at 0.05
+###   - count.tb which is a count table of a RNAseq experience which have in column : sample, and in row : gene
+###   - maxlogF : max Fold change in x axis which we set gene ID on geom point
+###   - minlogF : max Fold change in x axis which we set gene ID on geom point
+###   - minlogP : min Log10 in y which we set gene ID on geom point
+volcano.plot <-function(dds.results, is.anno=FALSE,anno,p.val=0.05,maxlogF=6,minlogF=0,minlogP=30,count.tb){
+  if(is.anno == TRUE){
+    dds.res <- dds.results %>% mutate(sig=padj<p.val) %>%  arrange(padj) %>%
+      inner_join(anno,by=c("row"=count.tb[1]))
+    return(ggplot(dds.res, aes(x=log2FoldChange, y=-log10(pvalue), col=sig)) +
+             geom_point() +
+             ggtitle("Volcano plot labelling top significant genes") +
+             geom_text_repel(data = subset(dds.res, (-log10(pvalue) > minlogP | log2FoldChange > maxlogF | log2FoldChange < minlogF)),
+                             aes(label = symbol),
+                             size = 4,
+                             box.padding = unit(0.35, "lines"),
+                             point.padding = unit(0.3, "lines"), color = "darkblue") +
+             scale_colour_discrete(name="",
+                                   labels=c("Not significative", "Significative", "NA")) +
+             guides(color = guide_legend(override.aes = list(size=5))) +
+             geom_vline(xintercept=0,linetype="dashed", color = "red")+
+             theme(legend.text=element_text(size=13)) +
+             theme(axis.title.x = element_text(size=14)) +
+             theme(axis.title.y = element_text(size=14)))
+  }else{
+    dds.res <- dds.results %>% mutate(sig=padj<p.val) %>%  arrange(padj)
+    return(ggplot(dds.res, aes(x=log2FoldChange, y=-log10(pvalue), col=sig)) +
+             geom_point()+
+             scale_colour_discrete(name="",
+                                   labels=c("Not significative", "Significative", "NA")) +
+             geom_vline(xintercept=0,linetype="dashed", color = "red") +
+             guides(colour = guide_legend(override.aes = list(size = 5))) +
+             theme(legend.text=element_text(size=13))+
+             theme(axis.title.x = element_text(size=14)) +
+             theme(axis.title.y = element_text(size=14))) 
+  }
+}
+
+### PCA ----
+### pca.plot generate a pca plot
+### pca.plot need 2 arguments
+###   - dds.resTransf which is an DESeq2 transformate object with vst() or rLogtransformation()
+pca.plot <- function(dds.resTransf,intgroup){
+  return(
+    plotPCA(dds.resTransf, intgroup=intgroup) +
+      theme(axis.title.x = element_text(size=14)) +
+      theme(axis.title.y = element_text(size=14)) +
+      scale_colour_discrete(name="")+
+      guides(colour = guide_legend(override.aes = list(size = 5))) +
+      theme(legend.text=element_text(size=13))+
+      geom_text_repel(
+        aes(label = colnames(dds.resTransf)),
+        size = 3,
+        box.padding = unit(0.35, "lines"),
+        point.padding = unit(0.3, "lines"), color = "darkblue")
+  )
+}
+
+### Distance matrix ----
+### distance.matrix.heatmap generate a heatmap of dist between different sample
+### distance.matrix.heatmap need just one argument
+###   - dds.resTransf which is an DESeq2 transformate object with vst() or rLogtransformation()
+distance.matrix.heatmap <- function(dds.resTransf){
+  dists <- dist(t(assay(dds.resTransf)))
+  mat <- as.matrix(dists)
+  hmcol=colorRampPalette(brewer.pal(9,"GnBu"))(100)
+  return(heatmap.2(mat,trace="none",col = rev(hmcol),margin=c(13,13)))
+}
+
+### Heatmap ----
+### gene.expression.heatmap generate a gene expression distance heatmap
+### gene.expression.heatmap need 10 argument
+###   - dds.results which is a results table obtain by function results() on a DESeq() object
+###   - is.anno if there is an annotation file
+###   - anno an annotation 
+###   - p.val which is pvalue accept un DE analysis, padje is set at 0.05
+###   - count.tb which is a count table of a RNAseq experience which have in column : sample, and in row : gene
+###   - metadata which a design table of an RNA-seq experience
+###   - dds.resTransf which is an DESeq2 transformate object with vst() or rLogtransformation()
+###   - condition : design formula of RNA-seq experience
+###   - min : num of row in results table starting from the top
+###   - max : num of row in results table starting from the top
+gene.expression.heatmap <- function(dds.results, dds.resTransf,is.anno=FALSE,anno,p.val=0.05,metadata,condition,count.tb,min,max){
+  res <- tbl_df(dds.results)
+  if(is.anno==TRUE){
+    res <- res %>% 
+      arrange(padj) %>% 
+      inner_join(anno,by=c("row"=count.tb[1])) %>%
+      filter(padj<p.val)
+    
+    NMF::aheatmap(assay(dds.resTransf)[arrange(res, padj, pvalue)$row[min:max],], 
+                  labRow=arrange(res, padj, pvalue)$symbol[min:max], 
+                  scale="row", distfun="pearson", 
+                  annCol=dplyr::select(metadata, condition), 
+                  col=c("green","black","black","red"))
+    
+  }else{
+    res <- res %>% 
+      arrange(padj) %>% filter(padj<p.val)
+    
+    NMF::aheatmap(assay(dds.resTransf)[arrange(res, padj, pvalue)$row[min:max],], 
+                  labRow=arrange(res, padj, pvalue)$symbol[min:max], 
+                  scale="row", distfun="pearson", 
+                  annCol=dplyr::select(metadata, condition), 
+                  col=c("green","black","black","red"))
+  }
+}
+
