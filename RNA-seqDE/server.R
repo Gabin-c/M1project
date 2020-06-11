a3003a590631c19f8baa3debe91aed544706a162
Gabin-c <gabincoudray.pro@gmail.com>
2020-06-11 21:48:07 +0200

    choix reference

--- RNA-seqDE/server.R
+++ RNA-seqDE/server.R
@@ -71,7 +71,11 @@ server <- function(input, output,session) {
   
   ### Update selectinput with columns of notUniqueValue() for the DESeq2 design
   observeEvent(input$MetadataFile,{
-    updateSelectInput(session,"DesignDESeq2", choices = paste("~ ",paste(colnames(notUniqueValue()))))
+    updateSelectInput(session,"DesignDESeq2", choices = paste(colnames(notUniqueValue())))
+  })
+  
+  observeEvent(input$DesignDESeq2,{
+    updateSelectInput(session,"Reference", choices = metadata()[,input$DesignDESeq2])
   })
   
 
@@ -116,9 +120,11 @@ server <- function(input, output,session) {
     waiter$show()
     
     ### DESeq2 process 
-    dds$dds <- DESeqDataSetFromMatrix(count_table(),colData=metadata(),design=as.formula(input$DesignDESeq2), tidy=TRUE)
+    dds$dds <- DESeqDataSetFromMatrix(count_table(),colData=metadata(),design=as.formula(paste("~",paste(input$DesignDESeq2))), tidy=TRUE)
     dds$DESeq2 <- DESeq(dds$dds)
     dds$results <- results(dds$DESeq2,tidy=TRUE)
+    colData(dds$dds)$input$DesignDESeq2 <- relevel(as.factor(input$DesignDESeq2) , ref = input$Reference)
+    
     
     ### Display success message after running DESeq2
     output$SuccessMessage <- renderUI({
